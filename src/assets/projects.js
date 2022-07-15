c482d7d9d280005e9824ce3c064619a8af571c47
unknown <mandarbhide62@gmail.com>
2022-07-15 19:00:39 +0530

    UPDATE150720221900

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,19 +1,19 @@
 export const projects = [
     {
+        title:"Web Microframework",
+        image:"backend.jpg",
+        description:"A backend microframework written in Dart language.",
+        technologies:["Dart"],
+        codeLink:"In Progress",
+        type:"Personal Project"
+    },
+    {
         title:"Stock price prediction",
         image:"stock.jpg",
         description:"One-day ahead prediction of closing price of specific stock using technical indicators and deep learning techniques.",
         technologies:["Python","Tensorflow"],
-        codeLink:"In Progress",
+        codeLink:"https://github.com/mdb2301/stock-prediction",
         type:"Final Year Project"
-    },
-    {
-        title:"Alumni App",
-        image:"alumni.png",
-        description:"A cross-platform mobile application containing authentication, profile, public posts and in-app-messaging.",
-        technologies:["Flutter","Firebase"],
-        codeLink:"https://play.google.com/store/apps/details?id=com.dpsrkapp.alumni",
-        type:"Freelance Project"
     },
     {
         title:"Book Exchange App",
@@ -22,21 +22,5 @@ export const projects = [
         technologies:["Flutter","Flask","Google Cloud"],
         codeLink:"https://github.com/mdb2301/comex",
         type:"Freelance Project"
-    },
-    {
-        title:"Product website",
-        image:"anubhooti.png",
-        description:"Website for deploying AI based products with authentication, subscriptions and usage limits.",
-        technologies:["Flask","SQL","Javascript"],
-        codeLink:"https://github.com/mdb2301/anubhooti-website",
-        type:"Internship"
-    },
-    {
-        title:"enLearn",
-        image:"enlearn.png",
-        description:"A design project for a designathon in team of two. Flutter implementation in progress.",
-        technologies:["Figma","Flutter"],
-        codeLink:"In Progress",
-        type:"Competition"
     }
 ]
